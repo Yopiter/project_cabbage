b7b9b5409a9b39a85e5ddd40c44939319de61ba9
casio888 <michi_mueller2@web.de>
2017-11-20 11:26:33 +0100

    Strings zu char[] in display

--- main/display.cpp
+++ main/display.cpp
@@ -1,18 +1,17 @@
-#include <string> //string, duh.
 #include <stdio.h> //sprintf
 using namespace std;
 
 //Auslesefunktionen der Pins in ein Headerfile "Schnittstelle"
 
-int Levelshit[][] = {{6,7}, {8,9,7}, {11,12,7} } // manövrieren mit ner grenze der tiefe des ersten arrays bei cklick gehts in die untere ebene und man weiß fürs zurück noch welches das oberstück ist. Für die 3. Ebene hab ich kp ehrlich :/ vlt wieder hart coden wie beim custBottom()
+int Levelshit[][] = {{6,7}, {8,9,7}, {11,12,7} } // manövrieren mit ner grenze der tiefe des ersten arrays bei click gehts in die untere ebene und man weiß fürs zurück noch welches das oberstück ist. Für die 3. Ebene hab ich kp ehrlich :/ vlt wieder hart coden wie beim custBottom()
 //7 wäre der zurück text
 
 string standardBottom = " <   select   > "; //wenn custBot.find(id) nicht true oder so, dann das hier nehmen
 string descriptions[x] = {"" }; //Liste aller oberen Texte -> Stelle im Array = id in anderen arrays / maps
 
 map<int,int> custBot={{7,0},{10,1},{14,2},{16,3},{17,4},{20,5},{21,5},{22,5}}; 
-string custBottom(int id) {
-  string output;
+char[] custBottom(int id) {
+  char[17] output;
   switch (id)
   {
     case 0: //temperatur
