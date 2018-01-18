5f7c0a3e920faefaa02a11bb818e207127c325fc
martin.schneider <martin.schneider@quodata.de>
2018-01-18 15:43:52 +0100

    Tones formatiert und leicht refactored

--- tests/Tones/Tones.ino
+++ tests/Tones/Tones.ino
@@ -1,24 +1,24 @@
 #include "pitches.h"
-int buzzPin=11; //                                     ||                                 |                                   ||
-int melody[]={NOTE_C6,0,NOTE_AS5,0,NOTE_GS5,0,NOTE_G5,0,NOTE_G5,NOTE_GS5,NOTE_GS5,NOTE_GS5,0,NOTE_G5,NOTE_GS5,NOTE_GS5,NOTE_GS5,0,NOTE_C6,0,NOTE_AS5,0,NOTE_GS5,0,NOTE_G5,0,NOTE_GS5,NOTE_GS5,NOTE_GS5,NOTE_C6,NOTE_AS5,NOTE_C6,NOTE_AS5,NOTE_GS5};
-long dur[]={8,8,8,8,8,8,8,8,16,16,16,16/3,8,16,16,16,16/3,8,8,8,8,8,8,8,8,8,16,16,16,16/13,16/14,16,16,1};
-int speed=2400;
-int M_Leng=sizeof(melody)/2;
-int pause_fest=25;
-float pause_flex=1;
+int buzzPin = 11; //                                     ||                                 |                                   ||
+int melody[] = {NOTE_C6, 0, NOTE_AS5, 0, NOTE_GS5, 0, NOTE_G5, 0, NOTE_G5, NOTE_GS5, NOTE_GS5, NOTE_GS5, 0, NOTE_G5, NOTE_GS5, NOTE_GS5, NOTE_GS5, 0, NOTE_C6, 0, NOTE_AS5, 0, NOTE_GS5, 0, NOTE_G5, 0, NOTE_GS5, NOTE_GS5, NOTE_GS5, NOTE_C6, NOTE_AS5, NOTE_C6, NOTE_AS5, NOTE_GS5};
+long dur[] = {8, 8, 8, 8, 8, 8, 8, 8, 16, 16, 16, 16 / 3, 8, 16, 16, 16, 16 / 3, 8, 8, 8, 8, 8, 8, 8, 8, 8, 16, 16, 16, 16 / 13, 16 / 14, 16, 16, 1};
+int speed = 2400;
+int M_Leng = sizeof(melody) / 2;
+int pause_fest = 25;
+float pause_flex = 1;
 void setup() {
   //Buzzer
-  pinMode(buzzPin,OUTPUT);
-  
+  pinMode(buzzPin, OUTPUT);
+
   int Length;
-  for (int i=0;i<M_Leng;i++){
-    Length=speed/dur[i];
-    tone(11,melody[i],Length);
-    delay(Length*pause_flex+pause_fest);
-    noTone(11);
+  for (int i = 0; i < M_Leng; i++) {
+    Length = speed / dur[i];
+    tone(buzzPin, melody[i], Length);
+    delay(Length * pause_flex + pause_fest);
+    noTone(buzzPin);
   }
 }
 
 void loop() {
-  
+
 }
